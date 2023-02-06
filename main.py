da40d9a13835869a12e5c050611fae61e5a5af60
KhongnatBaibua <khongnat.b@ku.th>
2023-02-06 23:15:52 +0700

    doing the reserve method and still struggling with the repitition of booking

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ from fastapi import FastAPI, HTTPException, Body
 from datetime import date
 from pymongo import MongoClient
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 
 DATABASE_NAME = "hotel"
 COLLECTION_NAME = "reservation"
-MONGO_DB_URL = "mongodb://localhost"
-MONGO_DB_PORT = 27017
+load_dotenv(".env")
+username = os.getenv("username")
+password = os.getenv("password")
 
 
 class Reservation(BaseModel):
     name : str
-    start_date: date
-    end_date: date
+    start_date: str
+    end_date: str
     room_id: int
 
 
-client = MongoClient(f"{MONGO_DB_URL}:{MONGO_DB_PORT}")
+client = MongoClient(f"mongodb://{username}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT")
 
-db = client[DATABASE_NAME]
+db = client["exceed08"]
 
-collection = db[COLLECTION_NAME]
+collection = db["oak_hw_day3"]
 
 app = FastAPI()
 
 
-def room_avaliable(room_id: int, start_date: str, end_date: str):
+def room_available(room_id: int, start_date: str, end_date: str):
     query={"room_id": room_id,
            "$or": 
                 [{"$and": [{"start_date": {"$lte": start_date}}, {"end_date": {"$gte": start_date}}]},
@@ -39,6 +42,12 @@ def room_avaliable(room_id: int, start_date: str, end_date: str):
     return not len(list_cursor) > 0
 
 
+
+
+
+
+
+
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
     pass
@@ -49,7 +58,18 @@ def get_reservation_by_room(room_id: int):
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    pass
+    room = room_available(reservation.room_id, reservation.start_date, reservation.end_date)
+    if room:
+        reservation_data ={
+                "name": f"{reservation.name}",
+                "room_id": f"{reservation.room_id}",
+                "start_date": f"{reservation.start_date}",
+                "end_date": f"{reservation.end_date}"
+            }
+        collection.insert_one(reservation_data)
+        return {"message": "reservation is complete"}
+    else:
+        return {"message": "reservation cannot be proceeded"}
 
 @app.put("/reservation/update")
 def update_reservation(reservation: Reservation, new_start_date: date = Body(), new_end_date: date = Body()):
@@ -58,3 +78,4 @@ def update_reservation(reservation: Reservation, new_start_date: date = Body(), 
 @app.delete("/reservation/delete")
 def cancel_reservation(reservation: Reservation):
     pass
+
